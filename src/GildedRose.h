d7769f5746f2fc1bfd5b292563086c2265dce090
Lesley <lesleylai@lesleylai.info>
2021-09-25 01:28:02 -0600

    Small C++ best practice changes

--- src/GildedRose.h
+++ src/GildedRose.h
@@ -1,21 +1,16 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
-class Item {
-public:
-  string name;
+struct Item {
+  std::string name;
   int sellIn;
   int quality;
-  Item(string name, int sellIn, int quality)
-      : name(name), sellIn(sellIn), quality(quality) {}
 };
 
 class GildedRose {
 public:
-  vector<Item> &items;
-  GildedRose(vector<Item> &items);
+  std::vector<Item> &items;
+  GildedRose(std::vector<Item> &items);
 
   void updateQuality();
 };
--- test/gildedrose_catch.cpp
+++ test/gildedrose_catch.cpp
@@ -10,8 +10,8 @@ std::ostream &operator<<(std::ostream &os, const Item &obj) {
 }
 
 TEST_CASE("UpdateQuality") {
-  vector<Item> items;
-  items.push_back(Item("foo", 0, 0));
+  std::vector<Item> items;
+  items.push_back(Item{"foo", 0, 0});
   GildedRose app(items);
   app.updateQuality();
   REQUIRE("fixme" == app.items[0].name);
