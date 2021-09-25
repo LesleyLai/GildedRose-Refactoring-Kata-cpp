95ca61f54a0c951cc6eb6ab423770c63c446cfbe
Lesley <lesleylai@lesleylai.info>
2021-09-25 01:24:20 -0600

    apply clang-format

--- src/GildedRose.h
+++ src/GildedRose.h
@@ -3,22 +3,19 @@
 
 using namespace std;
 
-class Item
-{
+class Item {
 public:
-    string name;
-    int sellIn;
-    int quality;
-    Item(string name, int sellIn, int quality) : name(name), sellIn(sellIn), quality(quality)
-    {}
+  string name;
+  int sellIn;
+  int quality;
+  Item(string name, int sellIn, int quality)
+      : name(name), sellIn(sellIn), quality(quality) {}
 };
 
-class GildedRose
-{
+class GildedRose {
 public:
-    vector<Item> & items;
-    GildedRose(vector<Item> & items);
+  vector<Item> &items;
+  GildedRose(vector<Item> &items);
 
-    void updateQuality();
+  void updateQuality();
 };
-
--- test/gildedrose_catch.cpp
+++ test/gildedrose_catch.cpp
@@ -1,18 +1,15 @@
-#include <catch2/catch.hpp>
 #include "ApprovalTests.hpp"
+#include <catch2/catch.hpp>
 
 #include "GildedRose.h"
 
-std::ostream& operator<<(std::ostream& os, const Item& obj)
-{
-  return os
-         << "name: " << obj.name
-         << ", sellIn: " << obj.sellIn
-         << ", quality: " << obj.quality;
+std::ostream &operator<<(std::ostream &os, const Item &obj) {
+  return os << "name: " << obj.name          //
+            << ", sellIn: " << obj.sellIn    //
+            << ", quality: " << obj.quality; //
 }
 
 TEST_CASE("UpdateQuality") {
-
   vector<Item> items;
   items.push_back(Item("foo", 0, 0));
   GildedRose app(items);
